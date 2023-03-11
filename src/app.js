95ddee4b40f932843ed7f19f972562e1b99283ef
shakibahadidi <127073032+shakibahadidi@users.noreply.github.com>
2023-03-11 19:10:44 +0330

    showing the current temperature

---
+++ src/app.js
@@ -0,0 +1,27 @@
+function displayTemperature(response){
+  console.log(response.data);
+let temperatureElement = document.querySelector("#temperature");
+let cityElement = document.querySelector("#city");
+let descriptionElement = document.querySelector("#description");
+let humidityElement = document.querySelector("#humidity");
+let windElement = document.querySelector("#wind");
+
+
+cityElement.innerHTML = response.data.name;
+temperatureElement.innerHTML= Math.round (response.data.main.temp);
+descriptionElement.innerHTML = response.data.weather[0].description;
+humidityElement.innerHTML = response.data.main.humidity;
+windElement.innerHTML = Math.round(response.data.wind.speed);
+
+
+
+}
+
+
+
+let apiKey = "dabebd47c695a280f2bdd7e93fd58481";
+let apiUrl =
+  `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+
+  axios.get(apiUrl).then(displayTemperature);
