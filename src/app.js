8487f675cd470d9d8f4ea358ca1877d466e6942e
shakibahadidi <127073032+shakibahadidi@users.noreply.github.com>
2023-03-13 16:33:15 +0330

    added metric units to API URL

--- src/app.js
+++ src/app.js
@@ -34,7 +34,7 @@ function formatDay(timestamp){
 
 
 function displayForecast(response) {
-  let forecast =response.data.daily
+  let forecast =response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   forecast.forEach(function (forecastDay , index) {
@@ -70,7 +70,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   let apiKey = "8c48afa47a9a9c24f3500c7039d50aaa";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&units=metric&appid=${apiKey}`;
   console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
